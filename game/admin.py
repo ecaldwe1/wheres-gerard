150dbfeacf056e8af224bb083c9042ef30aff8da
Jared M. Olson <jolson6@nd.edu>
2016-05-21 12:43:03 -0400

    Added the Puzzle and Answer models and migration. Added a requirements file. Note that Pillow is required for images.

--- game/admin.py
+++ game/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from game.models import Puzzle, Answer
 
-# Register your models here.
+
+class PuzzleAdmin(admin.ModelAdmin):
+    list_display = ('creator', 'submission_time', 'latitude', 'longitude')
+    list_filter = ('creator', 'submission_time')
+
+
+class AnswerAdmin(admin.ModelAdmin):
+    list_display = ('puzzle', 'is_correct', 'creator', 'submission_time', 'latitude', 'longitude')
+    list_filter = ('puzzle', 'is_correct', 'creator', 'submission_time')
+
+admin.site.register(Puzzle, PuzzleAdmin)
+admin.site.register(Answer, AnswerAdmin)
